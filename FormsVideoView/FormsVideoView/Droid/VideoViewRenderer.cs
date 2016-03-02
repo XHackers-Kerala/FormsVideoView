636cd093c083acd38b8377829b2cbac47bf4a6ed
sreerajpr <sreeraj.pr@fingent.com>
2016-03-02 10:41:31 +0530

    Going to Migrate to Library Project

--- FormsVideoView/FormsVideoView/Droid/VideoViewRenderer.cs
+++ FormsVideoView/FormsVideoView/Droid/VideoViewRenderer.cs
@@ -4,7 +4,7 @@ using Xamarin.Forms;
 using FormsVideoView;
 using FormsVideoView.Droid;
 
-[assembly:ExportRenderer(typeof(VideoView), typeof(VideoViewRenderer))]
+[assembly:ExportRenderer(typeof(FormsVideoView.VideoView), typeof(VideoViewRenderer))]
 namespace FormsVideoView.Droid
 {
 	public class VideoViewRenderer: ViewRenderer<VideoView, Android.Widget.VideoView> 
@@ -30,6 +30,21 @@ namespace FormsVideoView.Droid
 			videoView.Start ();
 
 			SetNativeControl (videoView);
+		}
+
+		protected override void OnElementPropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged (sender, e);
+
+			if (this.Element == null || this.Control == null)
+				return;
+
+			if (e.PropertyName == VideoView.MediaFileProperty.PropertyName) {
+				if(Element.MediaFileItem.IsStreaming)
+					Control.SetVideoURI ( Android.Net.Uri.Parse (Element.MediaFileItem.FileName));
+				else
+					Control.SetVideoURI ( Android.Net.Uri.FromFile( new Java.IO.File(Element.MediaFileItem.FileName)));
+			}
 		}
 
 	}
